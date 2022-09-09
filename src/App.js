db8da080f1366de0a23121418852cff55229376d
AtumaDavid <atumadavid35@gmail.com>
2022-09-09 16:55:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-// import Payment from "./components/payments/Payment";
-// import Success from "./components/success/Success";
+import Payment from "./components/payments/Payment";
+import Success from "./components/success/Success";
 import Error from "./components/error/Error";
 
 function App() {
   return (
     <div className="App">
-      {/* <Payment /> */}
-      {/* <Success /> */}
+      <Payment />
+      <Success />
       <Error />
     </div>
   );
